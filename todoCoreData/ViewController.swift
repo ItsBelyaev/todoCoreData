ebda0bc48e5ee3c0c374a368e84e5c4907f2c06c
down ultra <67225488+ultradown@users.noreply.github.com>
2021-06-20 16:16:01 +0300

    Initial Commit

---
+++ todoCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoCoreData
+//
+//  Created by Daniil Belyaev on 20.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
